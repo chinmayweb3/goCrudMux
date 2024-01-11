fa53f2699b23396a0ff6a7c31808df56263757f0
chinmayweb3 <chinmayweb3@gmail.com>
2024-01-11 21:22:52 +0530

    get Random handler aadded

--- handlers/getrandom.go
+++ handlers/getrandom.go
@@ -1,11 +1,26 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
+	"strings"
 )
 
 func GetRandom(w http.ResponseWriter, r *http.Request) {
+	url := "https://api.quotale.io/randofkfms"
+	resp, err := http.Get(url)
 
-	w.Write([]byte("running well "))
+	if err != nil || strings.HasPrefix(resp.Status, "4") {
+		w.Write([]byte("No quotes found"))
+		return
+	}
+	body, _ := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	var data map[string]string
+	json.Unmarshal(body, &data)
+
+	w.Write([]byte(data["content"]))
 
 }
--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", handlers.GetRandom).Methods("GET")
-	router.HandleFunc("/movies", handlers.GetMovies).Methods("GET")   // done
-	router.HandleFunc("/movie", handlers.GetMovie).Methods("GET")     // done
-	router.HandleFunc("/addmovie", handlers.AddMovie).Methods("POST") // done
-	router.HandleFunc("/deletemovie", handlers.DeleteMovie).Methods("DELETE")
+	router.HandleFunc("/movies", handlers.GetMovies).Methods("GET")           // done
+	router.HandleFunc("/movie", handlers.GetMovie).Methods("GET")             // done
+	router.HandleFunc("/addmovie", handlers.AddMovie).Methods("POST")         // done
+	router.HandleFunc("/deletemovie", handlers.DeleteMovie).Methods("DELETE") // done
 
 	fmt.Println("server is listening on : 8080")
 	if err := http.ListenAndServe(":8080", router); err != nil {
